706190d3382056a919becdd342762607a4c30ada
Jean Carlos Reyes Encarnación <20210546@itla.edu.do>
2022-09-16 01:39:58 -0400

    Brand Controller

---
+++ CarAPI/Controllers/BrandController.cs
@@ -0,0 +1,144 @@
+﻿using CarAPI.Core.Application.DTOS.Brand;
+using CarAPI.Core.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly IBrandServices _brandServices;
+        public BrandController(IBrandServices brandServices)
+        {
+            _brandServices = brandServices;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BrandDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get() {
+
+            try
+            {
+                var brands = await _brandServices.GetBrandWithInclude();
+
+                if (brands.Count == 0 || brands == null)
+                {
+                    return NotFound();
+                }
+
+
+                return Ok(brands);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(BrandDTO))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetById(int id) {
+
+            try
+            {
+                var brands = await _brandServices.GetBrandWithInclude();
+
+                var brandById = brands.FirstOrDefault(c => c.Id == id);
+
+                if (brandById == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(brandById);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Post(CreateBrandDTO dTO) {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                await _brandServices.Add(dTO);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK,Type =typeof(CreateBrandDTO))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Put(int id,CreateBrandDTO dTO) {
+
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+
+                dTO.Id = id;
+
+                await _brandServices.Update(dTO,id);
+
+                return Ok(dTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(int id)
+        {
+
+            try
+            {
+
+                await _brandServices.Delete(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+
+            }
+
+        }
+    }
+}
